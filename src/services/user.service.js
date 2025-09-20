411833d295e59d860dd8bcd9a0aa487930383543
Visitha Nirmal Rajapaksha <visitha2001@gmail.com>
2025-09-20 22:06:59 +0530

    get all users api

---
+++ src/services/user.service.js
@@ -0,0 +1,19 @@
+import logger from '#config/logger.js'
+import { db } from '#config/database.js';
+import { users } from '#models/user.model.js';
+
+export const getAllUsers = async () => {
+    try {
+        return await db.select({
+            id: users.id,
+            name: users.name,
+            email: users.email,
+            role: users.role,
+            created_at: users.created_at,
+            updated_at: users.updated_at
+        }).from(users);
+    } catch (error) {
+        logger.error("Error fetching all users", error);
+        throw new Error("Error fetching all users");
+    }
+};
